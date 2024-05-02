333ca0fe5d7b304842c680535ae589d9ae1fa9b3
Bryce Evans <bae43@bae43@cornell.edu>
2024-05-02 13:51:28 -0700

    Add CI actions, loosen python version requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zshot',
+    name='catman',
     version='0.1',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'catman=zshot.cli:main',
+        ],
+    },
     install_requires=[
         'faiss-cpu==1.7.4',
         'numpy',
@@ -18,9 +23,10 @@ setup(
             'pytest-mock',
         ]
     },
-    entry_points={
-        'console_scripts': [
-            'zshot=zshot.main:main',
-        ],
-    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
 )
