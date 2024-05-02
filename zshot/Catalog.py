5ab7deec94400bfd38f6bea3d4c5f812c7e85614
Bryce Evans <bae43@bae43@cornell.edu>
2024-05-02 09:12:46 -0700

    implement catalog manager

--- zshot/Catalog.py
+++ zshot/Catalog.py
@@ -1,4 +1,8 @@
-"""Class for storing info about a photo catalog."""
+"""Class for storing info about a photo catalog.
+
+This class is explicitly a datatype for reading and writing to disk.
+It is not meant to be updated in memory during runtime.
+"""
 
 import torch
 from dataclasses import dataclass, field
@@ -6,11 +10,10 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Catalog:
-    path: str
-    model_name: str
-    vector_dims: int
+    engine_name: str
+    vector_dimensions: int
     vectors: torch.Tensor = field(init=False)
     image_paths: list = field(default_factory=list, init=False)
 
     def __post_init__(self):
-        self.vectors = torch.zeros(0, self.vector_dims)
+        self.vectors = torch.zeros(0, self.vector_dimensions)
--- zshot/CatalogManager.py
+++ zshot/CatalogManager.py
@@ -1,37 +1,78 @@
-"""Class for managing a catalogue. Adding to it, fetching from it, and more.
-"""
+"""Class for managing a catalogue. Adding to it, fetching from it, and more."""
 
-import torch
+import logging
+import pickle
 
+import Catalog
 import ZShotEngine
 import ZShotDatabase
 
 
 class CatalogManager():
-    def __init__(self, clip_model=None, dimensions=None):
-        engine_kwargs = {}
-        if clip_model is not None:
-            engine_kwargs['model'] = clip_model
-
-        db_kwargs = {}
-        if dimensions is not None:
-            db_kwargs['dimensions'] = dimensions
-
-        self.engine = ZShotEngine(**engine_kwargs)
-        self.db = ZShotDatabase(**db_kwargs)
+    def __init__(self, logger=logging.getLogger(__name__)):
+        self.logger = logger
+        self.active_catalog_path = None
+        self.active_engine = None
+        self.active_db = None
 
 
-    def write_to_disk(self, path):
-        vectors = self.db.get_all_vectors()
-        torch.save(vectors, path)
+    def new_catalog(self, path):
+        """Create a new catalog file.
+        
+        path: filename to write to.
+        """
+        self.active_catalog_path = path
+
+        self.active_engine = ZShotEngine()
+        self.active_db = ZShotDatabase()
+
+        self.write_to_disk()
 
 
     def load_from_disk(self, path):
-        if self.db.count() > 0:
-            self.logger.warn("Loading catalogue from disk. Overwriting existing data.")
+        """Loads a catalog from disk. 
+        Will throw a FileNotFound Exception if path is not valid.
+        """
+        if self.active_catalog_path != None:
+            self.logger.warn("Replacing existing catalog data.")
+
+        with open(path, 'rb') as f:
+            catalog_data = pickle.load(f)
+            catalog = Catalog(**catalog_data)
+            
+            self.active_engine = ZShotEngine(catalog.engine_name)
+            self.active_db = ZShotDatabase(catalog.vector_dimensions)
+            self.active_db.add(catalog.image_paths, catalog.vectors)
+            self.active_catalog_path = path
+
+            self.logger.info(f"Successfully loaded catalog {path}")
+
+
+    def write_to_disk(self, path=None):
+        """Writes catalog data out to the active catalog path. 
+        Optionally set argument `path` to create a copy in a new location.
+        """
+        if path == None:
+            if self.active_catalog_path == None:
+                self.logger.error("No active catalog.")
+                return
+            
+            path = self.active_catalog_path
         
-        self.db = ZShotDatabase
-        loaded_tensor = torch.load(path)
+        catalog = Catalog(engine_name=self.active_engine.model_name, 
+                          vector_dimensions=self.active_db.dimension,
+                          vectors=self.active_db.get_all_vectors(),
+                          image_paths=self.active_db.paths())
+
+        with open(path, 'wb') as f:
+            pickle.dump(catalog, f)
+            self.logger.info("Success writing catalog {path}")
+
+
+
+
+        
+        
 
 
         
