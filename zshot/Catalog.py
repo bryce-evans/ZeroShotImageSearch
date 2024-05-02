0b84176a0b83881bba0501b385405c97366188a0
Bryce Evans <bae43@bae43@cornell.edu>
2024-05-02 12:06:29 -0700

    catalog and manager working better

--- zshot/Catalog.py
+++ zshot/Catalog.py
@@ -4,16 +4,17 @@ This class is explicitly a datatype for reading and writing to disk.
 It is not meant to be updated in memory during runtime.
 """
 
-import torch
+import faiss
 from dataclasses import dataclass, field
 
 
 @dataclass
 class Catalog:
     engine_name: str
-    vector_dimensions: int
-    vectors: torch.Tensor = field(init=False)
-    image_paths: list = field(default_factory=list, init=False)
+    dimension: int = field(default=512)
+    index: faiss.IndexFlatIP = field(default=None)
+    image_paths: list = field(default_factory=list)
 
     def __post_init__(self):
-        self.vectors = torch.zeros(0, self.vector_dimensions)
+        if self.index is None:
+            self.index = faiss.IndexFlatIP(self.dimension)
--- zshot/CatalogManager.py
+++ zshot/CatalogManager.py
@@ -1,14 +1,16 @@
-"""Class for managing a catalogue. Adding to it, fetching from it, and more."""
+"""Manager class that wraps the engine and DB, along with helpers for reading and writing to disk."""
 
 import logging
+import glob
 import pickle
+from typing import List
+from PIL import Image
+from typing import Optional
 
-import Catalog
-import ZShotEngine
-import ZShotDatabase
+from zshot import Catalog, ZShotEngine, ZShotDatabase
 
 
-class CatalogManager():
+class CatalogManager:
     def __init__(self, logger=logging.getLogger(__name__)):
         self.logger = logger
         self.active_catalog_path = None
@@ -16,7 +18,31 @@ class CatalogManager():
         self.active_db = None
 
 
-    def new_catalog(self, path):
+    def add_image(self, path: str):
+        """
+        Adds a single image path to the catalog. 
+        Works with local images and remote URLs.
+        File handling (check if file exists) is handled by the engine.
+        """
+        embeddings = self.active_engine.process_images_by_paths([path])
+        self.active_db.add([path], embeddings)
+        self.logger.info(f"Successfully added image at {path}.\n"
+                        f"Catalog Total: {self.active_db.count()}")
+        
+
+    def add_directory(self, dir: str):
+        """
+        Recursively adds all images in a directory to the database.
+        Only supports jpg for now.
+        """
+        jpg_files = glob.glob(f'{dir}/**/*.jpg', recursive=True)
+        embeddings = self.active_engine.process_images_by_paths(jpg_files)
+        self.active_db.add(jpg_files, embeddings)
+        self.logger.info(f"Successfully added {len(jpg_files)} additional images.\n"
+                         f"Catalog Total: {self.active_db.count()}")
+    
+
+    def new_catalog(self, path: str):
         """Create a new catalog file.
         
         path: filename to write to.
@@ -29,26 +55,21 @@ class CatalogManager():
         self.write_to_disk()
 
 
-    def load_from_disk(self, path):
+    def load_from_disk(self, path: str):
         """Loads a catalog from disk. 
         Will throw a FileNotFound Exception if path is not valid.
         """
-        if self.active_catalog_path != None:
-            self.logger.warn("Replacing existing catalog data.")
-
         with open(path, 'rb') as f:
-            catalog_data = pickle.load(f)
-            catalog = Catalog(**catalog_data)
-            
+            catalog = pickle.load(f)
             self.active_engine = ZShotEngine(catalog.engine_name)
-            self.active_db = ZShotDatabase(catalog.vector_dimensions)
-            self.active_db.add(catalog.image_paths, catalog.vectors)
+            self.active_db = ZShotDatabase(catalog.dimension)
+            self.active_db.index = catalog.index
             self.active_catalog_path = path
 
             self.logger.info(f"Successfully loaded catalog {path}")
 
 
-    def write_to_disk(self, path=None):
+    def write_to_disk(self, path: Optional[str]=None):
         """Writes catalog data out to the active catalog path. 
         Optionally set argument `path` to create a copy in a new location.
         """
@@ -60,19 +81,10 @@ class CatalogManager():
             path = self.active_catalog_path
         
         catalog = Catalog(engine_name=self.active_engine.model_name, 
-                          vector_dimensions=self.active_db.dimension,
-                          vectors=self.active_db.get_all_vectors(),
-                          image_paths=self.active_db.paths())
+                          index=self.active_db.index,
+                          image_paths=self.active_db.paths)
 
         with open(path, 'wb') as f:
             pickle.dump(catalog, f)
-            self.logger.info("Success writing catalog {path}")
-
-
-
-
-        
-        
-
-
+            self.logger.info(f"Success writing catalog {path}")
         
