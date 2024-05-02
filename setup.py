31c21eabc592f7c57f98cfbdc3155933008f50f8
Bryce Evans <bae43@bae43@cornell.edu>
2024-05-01 19:03:01 -0700

    fixed up ignore and installation setup

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'transformers',
-        'Pillow',
-        'faiss-cpu', 
+        'faiss-cpu',
         'numpy',
-        'glob2',
+        'pillow',
+        'pytorch',
+        'transformers',
+        
     ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+        ]
+    },
     entry_points={
         'console_scripts': [
             'zshot=zshot.main:main',
